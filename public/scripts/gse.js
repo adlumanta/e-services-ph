e1bc1a8c3f7ba0a12c75bc79359ba266ffd255ed
Angelo Rey Lumanta <adlumanta@up.edu.ph>
2019-07-08 00:31:42 +0800

    final project

---
+++ public/scripts/gse.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '012484923418748013730:hm7t8eowvxy';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
